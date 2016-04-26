c1785e28eefdd861905763913cb0e6e99e89d1b7
Aanand Prasad <aanand.prasad@gmail.com>
2016-04-27 00:02:06 +0100

    Strip ANSI chars from samples

--- process_dir.sh
+++ process_dir.sh
@@ -7,6 +7,6 @@ processed=$2
 
 for path in $raw/*.html; do
   destination="$processed/$(basename $path)"
-  cat "$path" | python ids_to_urls.py | python remove_swallowing_tags.py > "$destination"
-  >&2 echo "Wrote $destination"
+  cat "$path" | sed -e $'s/\x1B\[0m//' | python ids_to_urls.py | python remove_swallowing_tags.py > "$destination"
+  # >&2 echo "Wrote $destination"
 done
