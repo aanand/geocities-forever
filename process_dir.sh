c57d69a05e142eb9e00686edf4c351e5db57bcd3
Aanand Prasad <aanand.prasad@gmail.com>
2016-04-28 15:36:45 +0100

    Write random filenames

--- process_dir.sh
+++ process_dir.sh
@@ -2,11 +2,46 @@
 
 set -e
 
-raw=$1
-processed=$2
+script_name="$0"
+verbose=0
+preserve_filenames=0
+posargs=()
 
-for path in $raw/*.html; do
-  destination="$processed/$(basename $path)"
-  cat "$path" | sed -e $'s/\x1B\[0m//' | python ids_to_urls.py | python remove_swallowing_tags.py > "$destination"
-  # >&2 echo "Wrote $destination"
+while [[ "$#" -gt 0 ]]; do
+  case "$1" in
+    "-v"|"--verbose")
+      verbose=1;;
+    "-p"|"--preserve-filenames")
+      preserve_filenames=1;;
+    *)
+      posargs+=("$1");;
+  esac
+
+  shift
+done
+
+if [[ "${#posargs[@]}" -ne 2 ]]; then
+  >&2 echo "Usage: $script_name [-v|--verbose] [-p|--preserve-filenames] SOURCE_DIR TARGET_DIR"
+  exit 1
+fi
+
+source_dir="${posargs[0]}"
+target_dir="${posargs[1]}"
+
+for path in $source_dir/*.html; do
+  if [[ "$preserve_filenames" -eq 1 ]]; then
+    destination="${target_dir}/$(basename $path)"
+  else
+    destination="${target_dir}/${RANDOM}${RANDOM}.html"
+  fi
+
+  cat "$path" \
+    | sed -e $'s/\x1B\[0m//' \
+    | python ids_to_urls.py \
+    | python remove_swallowing_tags.py \
+    > "$destination"
+
+  if [[ "$verbose" -eq 1 ]]; then
+    >&2 echo "Wrote $destination"
+  fi
 done
