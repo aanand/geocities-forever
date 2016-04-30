ace6814764ac00fcfea097b5df348a82886d5b61
Aanand Prasad <aanand.prasad@gmail.com>
2016-04-30 17:52:41 +0100

    Always preserve filenames when processing

--- process_dir.sh
+++ process_dir.sh
@@ -4,15 +4,12 @@ set -e
 
 script_name="$0"
 verbose=0
-preserve_filenames=0
 posargs=()
 
 while [[ "$#" -gt 0 ]]; do
   case "$1" in
     "-v"|"--verbose")
       verbose=1;;
-    "-p"|"--preserve-filenames")
-      preserve_filenames=1;;
     *)
       posargs+=("$1");;
   esac
@@ -29,11 +26,7 @@ source_dir="${posargs[0]}"
 target_dir="${posargs[1]}"
 
 for path in $source_dir/*.html; do
-  if [[ "$preserve_filenames" -eq 1 ]]; then
-    destination="${target_dir}/$(basename $path)"
-  else
-    destination="${target_dir}/${RANDOM}${RANDOM}.html"
-  fi
+  destination="${target_dir}/$(basename $path)"
 
   cat "$path" \
     | sed -e $'s/\x1B\[0m//' \
